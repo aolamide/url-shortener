18cf6aefe9b4bbe6b871675472fcbf35a26bbd01
Olamide <aboyejiolamide15@gmail.com>
2019-09-04 17:40:57 +0100

    add copy link to clipbord feature

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ const urlForm = document.getElementById('url-form');
 const originalUrl = document.getElementById('original-url');
 const uniqueName = document.getElementById('unique-name');
 const confirmation = document.getElementById('confirmation');
+const copyButton = document.getElementById('copyButton');
 
 const formSubmit = e => {
     e.preventDefault();
@@ -23,10 +24,18 @@ const formSubmit = e => {
             confirmation.innerText = response.msg
         }
         else {
-            confirmation.innerHTML = `Hooray!!! The link can now be visited through <a target='_blank' rel='noopener noreferer' href=${response.newUrl}>${response.newUrl}</a>`
+            confirmation.value = `Hooray!!! The link can now be visited through <a target='_blank' rel='noopener noreferer' href=${response.newUrl}>${response.newUrl}</a><br>`
         }
     })
     .catch(err => console.log('oops', err))
 }
 
+const copyText = ()  => {
+    confirmation.select();
+    confirmation.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the link: " + confirmation.value + " to clipboard");
+}
+
 urlForm.addEventListener('submit', formSubmit);
+copyButton.addEventListener('click', copyText);
