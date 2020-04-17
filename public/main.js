5b742f0f2247dde70c2103f7d9c2d2b64108d357
Olamide <aboyejiolamide15@gmail.com>
2020-04-17 17:50:18 +0100

    validate unique name before sending

--- public/main.js
+++ public/main.js
@@ -7,37 +7,41 @@ const copyButton = document.getElementById('copyButton');
 const status = document.getElementById('status');
 
 const formSubmit = e => {
+    confirmationShow.innerText = '';
     e.preventDefault();
-    status.innerHTML = '<button type="button" class="loader"></button>'
-    fetch('/new', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          originalUrl : originalUrl.value,
-          unique_name : uniqueName.value
+    if(!uniqueName.value.trim()) confirmationShow.innerText = 'Enter a unique name';
+    else {
+        status.innerHTML = '<button type="button" class="loader"></button>';
+        fetch('/new', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            originalUrl : originalUrl.value,
+            unique_name : uniqueName.value
+            })
         })
-    })
-    .then(data => data.json())
-    .then(response => {
-        status.innerHTML = '<button>SHORTEN</button>'
-        if(!response.ok){
-            confirmationShow.innerText = response.msg;
-        }
-        else {
-            e.target.reset()
-            confirmationShow.innerHTML = `Hooray!!! The link can now be visited through <a target="_blank" href="https://${response.newUrl}" rel = "noopener noreferer" > ${response.newUrl} </a>`
-            confirmation.value = 'https://' + response.newUrl;
-            copyButton.innerText = 'Copy Link';
-        }
-    })
-    .catch(err => {
-        console.log('oops', err);
-        status.innerHTML = '<button>SHORTEN</button>';
-        confirmationShow.innerText = 'Network error, retry'
-    })
+        .then(data => data.json())
+        .then(response => {
+            status.innerHTML = '<button>SHORTEN</button>'
+            if(!response.ok){
+                confirmationShow.innerText = response.msg;
+            }
+            else {
+                e.target.reset()
+                confirmationShow.innerHTML = `Hooray!!! The link can now be visited through <a target="_blank" href="https://${response.newUrl}" rel = "noopener noreferer" > ${response.newUrl} </a>`
+                confirmation.value = 'https://' + response.newUrl;
+                copyButton.innerText = 'Copy Link';
+            }
+        })
+        .catch(err => {
+            console.log('oops', err);
+            status.innerHTML = '<button>SHORTEN</button>';
+            confirmationShow.innerText = 'Network error, retry'
+        })
+    };
 }
 
 const copyText = ()  => {
