fb4cff72111001ef08abc41d03fbc6759a0e0019
Olamide <aboyejiolamide15@gmail.com>
2020-05-04 01:22:58 +0100

    route for getting all urls

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { openUrl } = require('../controllers/url');
+const { openUrl, getAll } = require('../controllers/url');
 
 //opening shortened url route
 router.get('/:unique_name', openUrl  );
 
+//get all 
+router.get('/get/urls', getAll);
 
 module.exports = router;
